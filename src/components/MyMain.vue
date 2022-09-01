7d2996b7cb1a704810d9ebce7de7e61cda266a6b
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-01 17:11:51 +0200

    added a v-if for flags

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -5,8 +5,10 @@
       <li v-for="(movie, index) in moviesList" :key="index">
         {{movie.title}}
         {{movie.original_title}}
-        {{movie.original_language}}
-        {{movie.average}}
+        <img v-if="usedFlags.includes(movie.original_language)" 
+        :src="require('@/assets/img/' + movie.original_language +'.png')"/>
+        <span v-else>{{movie.original_language}}</span>
+        {{movie.vote_average}}
       </li>
     </ul>
   </div>
@@ -17,10 +19,18 @@ export default {
 name: 'MyMain',
 props: {
   moviesList: Array
+},
+data(){
+  return {
+    usedFlags: ['en','it', 'fr']
+  }
 }
 }
 </script>
 
 <style scoped lang="scss">
-
+img{
+  height: 10px;
+  width:20px;
+}
 </style>
