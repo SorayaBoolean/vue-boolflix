47ac0c41cfb97a8907e06093acab4bc0c42edc04
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-02 13:07:14 +0200

    animation: rotation

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <section class="movies_section">
       <div class="movie_card">
+        <h2>Movies</h2>
         <ul>
           <li v-for="(movie, index) in moviesList" :key="index">
             <img :src=" 'https://image.tmdb.org/t/p/w342' + movie.poster_path"/>
@@ -10,7 +11,10 @@
               <h4>Titolo originale:</h4>{{movie.original_title}} 
               <h4>Voto:</h4>        
               <i v-for="n in 5" class="fa-star" :class="(n>=changeVote(movie.vote_average))?'fa-regular':'fa-solid'" :key="n"></i>
-          </div>
+              <div class="overview">
+                <h4>Overview:</h4>{{movie.overview}}
+              </div>
+            </div>
           </li>
        </ul>
       </div>
@@ -20,17 +24,21 @@
     <section class="tv_section">
       <div class="movies_section">
         <div class="movie_card">
+          <h2>Series</h2>
           <ul>
-          <li v-for="(show, index) in tvShowsList" :key="index">
-            <img :src=" 'https://image.tmdb.org/t/p/w342' + show.poster_path"/>
-            <div class="movie_description">
-              <h4>Titolo:</h4>{{show.name}}
-              <h4>Titolo originale:</h4>{{show.original_name}}
-              <h4>Voto:</h4>
-              <i v-for="n in 5" class="fa-star" :class="(n>=changeVote(show.vote_average))?'fa-regular':'fa-solid'" :key="n"></i>
+            <li v-for="(show, index) in tvShowsList" :key="index">
+              <img class="poster" :src=" 'https://image.tmdb.org/t/p/w342' + show.poster_path"/>
+                <div class="movie_description">
+                  <h4>Titolo:</h4>{{show.name}}
+                  <h4>Titolo originale:</h4>{{show.original_name}}
+                  <h4>Voto:</h4>
+                  <i v-for="n in 5" class="fa-star" :class="(n>=changeVote(show.vote_average))?'fa-regular':'fa-solid'" :key="n"></i>
+                <div class="overview">
+                  <h4>Overview:</h4>{{show.overview}}
+                </div>
             </div>
           </li>
-    </ul>
+          </ul>
 
         </div>
       </div>
@@ -59,41 +67,66 @@ methods: {
     return Math.ceil(vote / 2)
   }
 }}
-
 </script>
 
 <style scoped lang="scss">
   .container{
-    height: 100%;
+    height: 8000px;
     width: 100vw;
     background-color: rgb(57, 57, 57);
+
+  }
+
+   
+
+  h2{
+    color:white;
+    text-align: center;
   }
   .little_flag{
     height: 20px;
     width: 20px;
   }
-  .movies_section{
-    display:flex;
-  }
-  .movie_card{
-    ul li {
-      list-style: none;
-    }
-    .poster{
-      width:200px;
-      height:100%;
-    }
-  }
+
   .movie_card ul{
     display: flex;
+    justify-content: center;
     flex-wrap:wrap;
-    margin: auto;
 
     li{
-      margin: 10px;
+      margin: 20px;
+      height: 500px;
       width: calc(100% / 4);
-      border:1px solid violet;
+      list-style: none;
+      &:hover{
+          cursor:pointer;
+          transition: transform 0.8s;
+          transform-style: preserve-3d;
+          transform: rotateY(180deg);
+          opacity: 30%;
+        }
 
+        
+    }
+
+    .movie_description{
+      display:none;
+      position: relative;
+      bottom:500px;
+      left: 10px;
+      color:white;
+
+      &:hover{
+        display:block;
+      }
+
+        i {
+          color: yellow;
+        }
+    }
+
+    .overview {
+      margin-right: 10px;
     }
   
   }
